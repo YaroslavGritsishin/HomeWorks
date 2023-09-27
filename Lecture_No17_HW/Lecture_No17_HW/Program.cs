104cdc0a4efcd2542069e17e465dca8a7f3ca633
Yaroslav Gritsishin <gritsishinyar@gmail.com>
2023-09-27 23:19:30 +0300

    Создал наглядную модель работы остановки публикации событий

--- Lecture_No17_HW/Lecture_No17_HW/Program.cs
+++ Lecture_No17_HW/Lecture_No17_HW/Program.cs
@@ -1,9 +1,22 @@
 ﻿using Lecture_No17_HW_Core;
 
 CancellationTokenSource tokenSource = new CancellationTokenSource();
+string directoryPath = "D:\\Tests";
 using PathFinder pathFinder = new PathFinder();
+Console.WriteLine("Subscribe to FindPath event");
 pathFinder.Subscribe(args => Console.WriteLine(args.FileName));
-pathFinder.Execute("D:\\Tests", tokenSource.Token);
+Console.WriteLine($"Start find file of {directoryPath} directory!");
+pathFinder.Execute(directoryPath, tokenSource.Token);
+Console.WriteLine("End find File!");
+Console.WriteLine($"Return Start faind file of {directoryPath} directory!");
+pathFinder.Execute(directoryPath, tokenSource.Token);
+Console.WriteLine("End find File!");
+Console.WriteLine("Cancel publish events!");
+tokenSource.Cancel();
+Console.WriteLine($"Return Start faind file of {directoryPath} directory!");
+pathFinder.Execute(directoryPath, tokenSource.Token);
+Console.WriteLine("End find File!");
+Console.WriteLine("Program is finished!");
 Console.ReadLine();
 
 
