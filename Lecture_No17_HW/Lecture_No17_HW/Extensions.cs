a21426ad95d12365ee3ccd19694c3a4006f7e563
Yaroslav Gritsishin <gritsishinyar@gmail.com>
2023-09-27 10:12:42 +0300

    Добавил обощеную функцию, находящую максимальный элемент коллекции

---
+++ Lecture_No17_HW/Lecture_No17_HW/Extensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Lecture_No17_HW
+{
+    public static class Extensions
+    {
+        public static T GetMax<T>(this IEnumerable<T> e, Func<T, float> getParameter) where T : class
+        {
+            Dictionary<float, T> result = new();
+            foreach (var item in e) 
+                result[getParameter(item)] = item;
+            return result[result.Keys.Max()];
+        }
+    }
+}
