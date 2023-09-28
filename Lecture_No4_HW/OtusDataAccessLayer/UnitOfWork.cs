a067c2f5c25e6385d1f4d385ebe5b179fcd9f49c
Yaroslav Gritsishin <gritsishinyar@gmail.com>
2023-09-28 10:00:09 +0300

    Добавлены репозитории и реализован unitOfWork с помощью EF

---
+++ Lecture_No4_HW/OtusDataAccessLayer/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OtusDataAccessLayer.Abstractions;
+using OtusDataAccessLayer.Repositories;
+
+namespace OtusDataAccessLayer
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly ICourseRepository courseRepository;
+        private readonly DbContext context;
+
+        public UnitOfWork(DbContext context)
+        {
+            this.context = context;
+        }
+        public IStudentRepository StudentRepository => studentRepository?? new StudentRepository(context);
+        public ICourseRepository CourseRepository => courseRepository ?? new CourseRepository(context);
+
+        public async Task SaveChangesAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
