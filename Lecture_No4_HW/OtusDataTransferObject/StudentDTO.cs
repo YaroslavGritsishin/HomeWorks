c693c333bd2a11f0f9de73980abadfa1c2dc65e2
Yaroslav Gritsishin <gritsishinyar@gmail.com>
2023-09-28 11:25:01 +0300

    Добавил реализацию ДТО на основе АвтоМаппера в виде медодов расширения

---
+++ Lecture_No4_HW/OtusDataTransferObject/StudentDTO.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Lecture_No4_HW.ViewModels;
+using OtusDomain.Entities;
+
+namespace OtusDataTransferObject
+{
+    public static class StudentDTO
+    {
+        public static MapperConfiguration ToEntityMap() => new(cfg =>
+        {
+
+            cfg.CreateMap<StudentViewModel, StudentEntity>()
+                .ForMember(src => src.Name, opt => opt.MapFrom(target => target.Name))
+                .ForMember(src => src.Surname, opt => opt.MapFrom(target => target.Surname))
+                .ForMember(src => src.Age, opt => opt.MapFrom(target => target.Age))
+                .ForMember(src => src.Courses, opt => opt.MapFrom(target => target.Courses))
+                .ForMember(src => src.Id, opt => opt.MapFrom(target => target.Id));
+
+            cfg.CreateMap<CourseViewModel, CourseEntity>()
+                .ForMember(src => src.CourseName, opt => opt.MapFrom(target => target.CourseName))
+                .ForMember(src => src.Students, opt => opt.MapFrom(target => target.Students))
+                .ForMember(src => src.Id, opt => opt.MapFrom(target => target.Id));
+
+        });
+        public static MapperConfiguration ToViewModelMap() => new(cfg =>
+        {
+
+            cfg.CreateMap<StudentEntity, StudentViewModel>()
+                .ForMember(src => src.Name, opt => opt.MapFrom(target => target.Name))
+                .ForMember(src => src.Surname, opt => opt.MapFrom(target => target.Surname))
+                .ForMember(src => src.Age, opt => opt.MapFrom(target => target.Age))
+                .ForMember(src => src.Courses, opt => opt.MapFrom(target => target.Courses))
+                .ForMember(src => src.Id, opt => opt.MapFrom(target => target.Id));
+
+            cfg.CreateMap<CourseViewModel, CourseEntity>()
+                .ForMember(src => src.CourseName, opt => opt.MapFrom(target => target.CourseName))
+                .ForMember(src => src.Students, opt => opt.MapFrom(target => target.Students))
+                .ForMember(src => src.Id, opt => opt.MapFrom(target => target.Id));
+        });
+
+        public static StudentEntity ToEntity(this StudentViewModel source) => new Mapper(ToEntityMap()).Map<StudentViewModel,StudentEntity>(source);
+        public static IEnumerable<StudentEntity> ToEntity(this IEnumerable<StudentViewModel> source) => new Mapper(ToEntityMap())
+            .Map<IEnumerable<StudentViewModel>, IEnumerable<StudentEntity>>(source);
+
+        public static StudentViewModel ToViewModel(this StudentEntity source) => new Mapper(ToViewModelMap()).Map<StudentEntity, StudentViewModel>(source);
+        public static IEnumerable<StudentViewModel> ToViewModel(this IEnumerable<StudentEntity> source) => new Mapper(ToViewModelMap())
+            .Map<IEnumerable<StudentEntity>, IEnumerable<StudentViewModel>>(source);
+    }
+}
--- Lecture_No4_HW/OtusServices/Abstractions/IStudentService.cs
+++ Lecture_No4_HW/OtusServices/Abstractions/IStudentService.cs
@@ -1,6 +1,13 @@
-﻿namespace OtusServices.Abstractions
+﻿using Lecture_No4_HW.ViewModels;
+
+namespace OtusServices.Abstractions
 {
     public interface IStudentService
     {
+        Task CreateStudentAsync(StudentViewModel student);
+        Task DeleteStudentAsync(StudentViewModel student);
+        Task<StudentViewModel> FindStudent(Action<StudentViewModel> action);
+        Task<IEnumerable<StudentViewModel>> GetAllCoursesAsync();
+        Task UpdateStudent(StudentViewModel student);
     }
 }
