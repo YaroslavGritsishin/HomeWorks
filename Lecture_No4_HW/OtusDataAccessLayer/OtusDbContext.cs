5e7ec08c0d997c809cbe8efb1fab0307e7f67086
Yaroslav Gritsishin <gritsishinyar@gmail.com>
2023-09-28 10:40:10 +0300

    Добавлен контекст для БД

---
+++ Lecture_No4_HW/OtusDataAccessLayer/OtusDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OtusDomain.Entities;
+
+namespace OtusDataAccessLayer
+{
+    public class OtusDbContext: DbContext
+    {
+        public OtusDbContext(DbContextOptions<OtusDbContext> options): base(options) { }
+        DbSet<StudentEntity> Students { get; set; }
+        DbSet<CourseEntity> Courses { get; set; }
+    }
+}
