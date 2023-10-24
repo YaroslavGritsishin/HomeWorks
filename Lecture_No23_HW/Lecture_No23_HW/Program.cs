cf24bf1e65bc76b10bd01b4624a0408a334d8cf9
Yaroslav Gritsishin <grits.developer@gmail.com>
2023-10-25 00:46:57 +0300

    Выполнил домашнее задание по параллельному считыванию файлов

---
+++ Lecture_No23_HW/Lecture_No23_HW/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Stopwatch stopWatch = new();
+        List<Task> tasks = new();
+        
+        stopWatch.Start();
+        
+        foreach (var filePath in Directory.GetFiles("FILES_OTUS_HW"))
+            tasks.Add(Task.Run(() => ReadAndCountSpases(filePath))); 
+
+        await Task.WhenAll(tasks);
+        
+        stopWatch.Stop();
+
+        await Console.Out.WriteLineAsync(stopWatch.Elapsed.ToString(@"m\:ss\.ffff"));
+        Console.ReadLine();
+    }
+    public static void ReadAndCountSpases(string filePath)
+    {
+        var count = File.ReadAllText(filePath).Count(symbol => symbol == ' ');
+        Console.WriteLine($"File with name: {Path.GetFileName(filePath)} contains {count} spaces");
+    }
+
+
+}
