c780522774951ede0dc066ea730c6e3ba60929ca
YaroslavGritsishin <59379844+YaroslavGritsishin@users.noreply.github.com>
2023-11-21 22:49:00 +0300

    Lecture_No23_HW (#3)
    * Выполнил домашнее задание по параллельному считыванию файлов
    
    * Вынес подсчет пробелов в отдельную функцию, запустил чтение и подсчет паралельно
    
    ---------
    
    Co-authored-by: Yaroslav Gritsishin <grits.developer@gmail.com>

---
+++ Lecture_No23_HW/Lecture_No23_HW/Program.cs
@@ -0,0 +1,37 @@
+﻿using Lecture_No23_HW;
+using Lecture_No23_HW.Models;
+using System.Diagnostics;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Stopwatch stopWatch = new();
+        string directoryPath = "FILES_OTUS_HW";
+
+        stopWatch.Start();
+
+        await ParallelReadAndCountSpacesInFilesOfDirectory(directoryPath);
+         
+        stopWatch.Stop();
+
+        await Console.Out.WriteLineAsync(stopWatch.Elapsed.ToString(@"m\:ss\.ffff"));
+        Console.ReadLine();
+    }
+    public static void CountSpases(FileData fileData)
+    {
+        var count = fileData.Data?.Count(symbol => symbol == ' ');
+        Console.WriteLine($"File with name: {Path.GetFileName(fileData.FilePath)} contains {count} spaces");
+    }
+    public static FileData ReadFile(string filePath) => new FileData()
+    {
+        Data = File.ReadAllText(filePath),
+        FilePath = filePath
+    };
+    public static async Task ParallelReadAndCountSpacesInFilesOfDirectory(string dictionaryPath)
+    {
+        var filesPath = Directory.GetFiles(dictionaryPath);
+        var readFilesData = await filesPath.ExecuteInParallel(ReadFile);
+        await readFilesData.ExecuteInParallel(CountSpases);
+    }
+}
