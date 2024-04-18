da449b1a1f1ee633e194e234ee085de986a34bef
Yaroslav Gritsishin <gritsishinyar@gmail.com>
2024-04-18 09:25:52 +0300

    Добавлена сериализация на основе рефлексии

---
+++ Lecture_No13_HW/Lecture_No13_HW/CsvSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Text.Json;
+
+namespace Lecture_No13_HW
+{
+    public class CsvSerializer
+    {
+        public static string Serializer(object obj)
+        {
+            Type serializationObjectType = obj.GetType();
+            FieldInfo[] fields = serializationObjectType.GetFields();
+            return string.Join(',', fields.Select(f => f.GetValue(obj)));
+        }
+    }
+}
--- Lecture_No13_HW/Lecture_No13_HW/Program.cs
+++ Lecture_No13_HW/Lecture_No13_HW/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lecture_No13_HW;
+using System.Text.Json;
+
+F TestClassF = new()
+{
+    i1 = 1,
+    i2 = 2,
+    i3 = 3,
+    i4 = 4,
+    i5 = 5
+};
+
+var csv = CsvSerializer.Serializer(TestClassF);
+Console.WriteLine($"[{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")}] {csv}");
+
+var js = JsonSerializer.Serialize(TestClassF, new JsonSerializerOptions() { IncludeFields = true});
+Console.WriteLine($"[{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")}] {js}");
+
+Console.ReadKey();
